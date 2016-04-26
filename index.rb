24220e64801c10a456eaf335918f7532b3a5739d
Oskar Niburski <oskarniburski@gmail.com>
2016-04-25 23:55:51 -0400

    Working on xml structure

--- index.rb
+++ index.rb
@@ -4,22 +4,35 @@ require 'oauth'
 require 'quickbooks-ruby'
 
 m = HashThing.new()
-qb_oauth_consumer = OAuth::Consumer.new(m.qb_key, m.qb_secret, {
-    :site                 => "https://oauth.intuit.com",
-    :request_token_path   => "/oauth/v1/get_request_token",
-    :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
-    :access_token_path    => "/oauth/v1/get_access_token"
-})
+# qb_oauth_consumer = OAuth::Consumer.new(m.qb_key, m.qb_secret, {
+#     :site                 => "https://oauth.intuit.com",
+#     :request_token_path   => "/oauth/v1/get_request_token",
+#     :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
+#     :access_token_path    => "/oauth/v1/get_access_token"
+# })
+
+
+# request_token = qb_oauth_consumer.get_request_token
+
+# # access_token = request_token.get_access_token(:oauth_verifier => m.oauth_verifier)
+# m.request_token = request_token
 
 def get_hash(qb_oauth_consumer, m)
   qb_client = OAuth::AccessToken.new(qb_oauth_consumer, m.access_token, m.qb_secret)
-  
+
   qb_fields_hash = get_qb_fields(qb_client, m)
-  binding.pry
 end
 
-def get_qb_fields(qb_client, m)
-  report_service = Quickbooks::Service::Reports.new({access_token: qb_client, company_id: m.uid})
+def assumed_hash(m)
+  hash = m.hash
+  file_name = "qbo_data.xls"
+  stream = File.open("./views/qbo.xml.erb")
+  binding.pry
+  # send_data(stream, :type=>"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :filename => file_name)
+end
+
+def get_qb_fields(qb_client, m)  
+  report_service = Quickbooks::Service::Reports.new({access_token: qb_client, company_id: m.uid})  
   allQBFields = []
   terms = ['', 'This Month', 'Last Month', 'This Fiscal Quarter', 'Last Fiscal Quarter']
   fields = [{"BalanceSheet" => terms}, {"CashFlow" => terms}, {"ProfitAndLoss" => terms}, {'GeneralLedger' => terms}]
@@ -28,7 +41,7 @@ def get_qb_fields(qb_client, m)
     queryString = queries.keys[0]
     queryTerms = queries.values[0]
     queryTerms.each do |term|
-      if term != ""
+      if term != ""        
         report_service.query(queryString, term)
       else
         begin
@@ -46,4 +59,5 @@ def get_qb_fields(qb_client, m)
 
 end
 
-get_hash(qb_oauth_consumer, m)
+# get_hash(qb_oauth_consumer, m)
+assumed_hash(m)
